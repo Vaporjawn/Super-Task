650aad80755185659886a01f3d23ae240b175b19
Victor Williams <vrwilliams719@gmail.com>
2017-01-24 14:34:35 -0500

    wam

---
+++ src/pkgsuper/task/SuperTask.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgsuper.task;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author todoslosdays
+ */
+public class SuperTask {
+
+   Souble dub; 
+   double d = dub;
+   JOptionPane.
+
+    public SuperTask() {
+        this.dub = new Souble(5.0);
+    }
+   
+   
+}
